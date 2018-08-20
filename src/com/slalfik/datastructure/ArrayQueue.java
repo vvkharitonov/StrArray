21d617a1147d2d739c6d0dc8fc3f3da480dbec6c
DSK21 DSK <DSK21@ptc.local>
2018-08-20 12:07:13 +0300

    Queue taki added

--- src/com/slalfik/datastructure/ArrayQueue.java
+++ src/com/slalfik/datastructure/ArrayQueue.java
@@ -3,5 +3,43 @@ package com.slalfik.datastructure;
 /**
  * Created by dsk21 on 8/20/2018.
  */
-public class ArrayQueue {
+public class ArrayQueue implements Queue{
+    private final static int INIT_CAPACITY = 2;
+
+    Object[] array;
+    int size;
+
+    public ArrayQueue() {
+        array=new Object[INIT_CAPACITY];
+    }
+
+    @Override
+    public void enqueue(Object value) {
+        if (array.length == size ) {
+            Object[] newArray = new Object[size*2];
+            for (int i = 0; i <size ; i++) {
+                newArray[i]=array[i];
+            }
+            array=newArray;
+        }
+
+        array[size]=value;
+        size++;
+    }
+
+    @Override
+    public Object dequeue() {
+        Object result = array[0];
+        size--;
+        for (int i = 0; i < size; i++) {
+            array[i]=array[i+1];
+        }
+        return result;
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
--- src/com/slalfik/datastructure/Queue.java
+++ src/com/slalfik/datastructure/Queue.java
@@ -4,4 +4,12 @@ package com.slalfik.datastructure;
  * Created by dsk21 on 8/20/2018.
  */
 public interface Queue {
+
+
+    void enqueue(Object value);
+
+    Object dequeue();
+
+    int size();
+
 }
